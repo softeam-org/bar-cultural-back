384b29c1531eabe6e126f9032fa31c0695ef22b0
Jorge Loch <jorgembloch2001@gmail.com>
2024-04-25 20:27:43 -0300

    fix: removing unecessary injection in the product module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { CategoriesController } from '@src/categories/categories.controller';
-import { CategoriesService } from '@src/categories/categories.service';
 import { PrismaService } from '@src/prisma/prisma.service';
 
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  controllers: [CategoriesController, ProductsController],
-  providers: [CategoriesService, ProductsService, PrismaService],
+  controllers: [ProductsController],
+  providers: [ProductsService, PrismaService],
 })
 export class ProductsModule {}
