429c8c68b00d3d1ddb0fd44b5037ce6a35a93bd5
Arthur Lima <arthurhenrique.contactus@gmail.com>
2024-07-28 12:31:21 -0300

    feat: incomplete auth sigIn route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Post, Request } from '@nestjs/common';
 
+
+
+import { Seller } from '@src/sellers/entities/seller.entity';
+
 import { AuthService } from './auth.service';
 
+
+interface SellerForLogin extends  Omit<Seller, 'password'> {}
+interface RequestWithSeller extends Request{
+  seller: SellerForLogin;
+}
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService){}
 
   @Post('signin')
-  async login(@Request() req){
+  async login(@Request() req: RequestWithSeller){
     return this.authService.login(req.seller)
   }
 }
